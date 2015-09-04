f822d70d4d4f114c66beedfbb6a56d71f492fd16
cano <xch_554@163.com>
2015-09-05 07:11:14 +0800

    成功测试页面

---
+++ src/main/java/web/cano/spider/scheduler/RedisScheduler.java
@@ -0,0 +1,241 @@
+package web.cano.spider.scheduler;
+
+import com.google.gson.Gson;
+import org.apache.http.HttpHost;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+import web.cano.spider.Request;
+import web.cano.spider.Site;
+import web.cano.spider.Spider;
+import web.cano.spider.Task;
+import web.cano.spider.scheduler.component.DuplicateRemover;
+import web.cano.spider.utils.BaseDAO;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Use Redis as url scheduler for distributed crawlers.<br>
+ *
+ * @author code4crafter@gmail.com <br>
+ * @since 0.2.0
+ */
+public class RedisScheduler extends DuplicateRemovedScheduler implements MonitorableScheduler, DuplicateRemover {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    private JedisPool pool;
+
+    private Site site = null;
+
+    private boolean startOver = false;
+
+    private static final String QUEUE_PREFIX = "queue_";
+
+    private static final String QUEUE_PREFIX_DUPICATE = "queue_dupicate_";
+
+    private static final String SET_PREFIX = "set_";
+
+    private RedisScheduler(String host) {
+        JedisPoolConfig config = new JedisPoolConfig();
+        config.setMaxActive(1000);
+        config.setMaxIdle(20);
+        config.setMaxWait(100000l);
+        this.pool = new JedisPool(config, host,6379,100000);
+        setDuplicateRemover(this);
+    }
+
+    public RedisScheduler(String host, Site site){
+        this(host);
+        this.site = site;
+    }
+
+    @Override
+    public boolean isDuplicate(Request request, Task task) {
+        Jedis jedis = pool.getResource();
+        try {
+            if(startOver){
+                jedis.del(getSetKey(task));
+                jedis.del(getQueueKey(task));
+                jedis.del(getDupicateQueueKey(task));
+                startOver = false;
+                logger.info("remove redis queue and start over parsing.");
+            }
+
+            boolean isDuplicate = jedis.sismember(getSetKey(task), request.getUrl());
+            if (!isDuplicate || request.isRefresh()) {
+                jedis.sadd(getSetKey(task), request.getUrl());
+                return false;
+            }
+            return true;
+        } finally {
+            pool.returnResource(jedis);
+        }
+
+    }
+
+    @Override
+    protected void pushWhenNoDuplicate(Request request, Task task) {
+        Jedis jedis = pool.getResource();
+        try{
+            Gson gson = new Gson();
+            String json = gson.toJson(request);
+            if(site.isDeepFirst()){
+                jedis.lpush(getQueueKey(task), json);
+            }else {
+                jedis.rpush(getQueueKey(task), json);
+            }
+        } finally {
+            pool.returnResource(jedis);
+        }
+    }
+
+    @Override
+    public synchronized Request poll(Task task) {
+        Jedis jedis = pool.getResource();
+        try{
+            String json = jedis.lpop(getQueueKey(task));
+            jedis.rpush(getDupicateQueueKey(task), json);
+
+            if (json == null){
+                return null;
+            }
+            Gson gson = new Gson();
+            Request request = gson.fromJson(json, Request.class);
+            return request;
+        }finally {
+            pool.returnResource(jedis);
+        }
+    }
+
+    @Override
+    public void completeParse(Request request, Task task) {
+        Jedis jedis = pool.getResource();
+
+        try{
+            Gson gson = new Gson();
+
+            HttpHost httpHost = request.getProxy();
+            int statusCode = request.getStatusCode();
+            request.setProxy(null);
+            request.setStatusCode(0);
+            String json = gson.toJson(request);
+            request.setProxy(httpHost);
+            request.setStatusCode(statusCode);
+
+            jedis.lrem(getDupicateQueueKey(task),1,json);
+
+        }finally {
+            pool.returnResource(jedis);
+        }
+    }
+
+    protected String getSetKey(Task task) {
+        return SET_PREFIX + ((Spider) task).getUUID();
+    }
+
+    protected String getQueueKey(Task task) {
+        return QUEUE_PREFIX + ((Spider) task).getUUID();
+    }
+
+    protected String getDupicateQueueKey(Task task) {
+        return QUEUE_PREFIX_DUPICATE + ((Spider) task).getUUID();
+    }
+
+    @Override
+    public int getLeftRequestsCount(Task task) {
+        Jedis jedis = pool.getResource();
+        try {
+            Long size = jedis.llen(getQueueKey(task));
+            return size.intValue();
+        } finally {
+            pool.returnResource(jedis);
+        }
+    }
+
+    @Override
+    public int getTotalRequestsCount(Task task) {
+        Jedis jedis = pool.getResource();
+        try {
+            Long size = jedis.scard(getQueueKey(task));
+            return size.intValue();
+        } finally {
+            pool.returnResource(jedis);
+        }
+    }
+
+    public RedisScheduler setStartOver(boolean startOver) {
+        this.startOver = startOver;
+        return this;
+    }
+
+    @Override
+    public List<Request> checkIfCompleteParse(Task task) {
+        Jedis jedis = pool.getResource();
+        Gson gson = new Gson();
+        List<Request> list = new ArrayList<Request>();
+        try{
+            String key = getDupicateQueueKey(task);
+            List<String> requests = jedis.lrange((key), 0, jedis.llen(key));
+            for(String json : requests){
+                Request request = gson.fromJson(json, Request.class);
+                list.add(request);
+            }
+            return list;
+        }finally {
+            pool.returnResource(jedis);
+        }
+    }
+
+    @Override
+    public void saveQueue(Task task) {
+        Jedis jedis = pool.getResource();
+        BaseDAO dao = BaseDAO.getInstance("cano");
+
+        //create table
+        String tableName = task.getUUID() + "UrlSet";
+        String sql = "DROP TABLE IF EXISTS `" + tableName + "`;";
+        dao.executeUpdate(sql);
+        logger.info("drop table " + tableName + " and re-recreate again.");
+        logger.info("creating table " + tableName);
+        sql = "CREATE TABLE IF NOT EXISTS `" + tableName + "` (`id` int(11) NOT NULL AUTO_INCREMENT,`url` varchar(1024) NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB;";
+        dao.executeUpdate(sql);
+
+        //store values
+        logger.info("storing parsed urls.");
+        try{
+            Set<String> urlSet = jedis.smembers(getSetKey(task));
+            for(String url : urlSet){
+                logger.info(url);
+                sql = "INSERT INTO `" + tableName + "` (`id`,`url`) VALUES (NULL,'" + url + "')";
+                dao.executeUpdate(sql);
+            }
+        }finally {
+            pool.returnResource(jedis);
+        }
+    }
+
+    @Override
+    public void recoverQueue(Task task) {
+        BaseDAO dao = BaseDAO.getInstance("cano");
+        Jedis jedis = pool.getResource();
+
+        //get urls
+        String tableName = task.getUUID() + "UrlSet";
+        String sql = "select url from " + tableName;
+        try {
+            List<Map<String,Object>> list = dao.executeQuery(sql,null,null);
+            for (Map<String, Object> map : list) {
+                String url = (String) map.get("url");
+                jedis.sadd(getSetKey(task), url);
+            }
+        }finally {
+            pool.returnResource(jedis);
+        }
+    }
+}
---
+++ src/main/java/web/cano/spider/utils/BaseDAO.java
@@ -0,0 +1,153 @@
+package web.cano.spider.utils;
+
+/**
+ * Created by cano on 2015/1/20.
+ */
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BaseDAO {
+    private final static String DRIVER = "com.mysql.jdbc.Driver";
+    private final static String URL = "jdbc:mysql://127.0.0.1:3306/";
+    private final static String USERNAME = "root";
+    private final static String PASSWORD = "";
+
+    private static Map<String,BaseDAO> instances = new HashMap<String,BaseDAO>();
+    private Connection conn = null;
+    private String dbName = null;
+
+    private void BaseDAO(){ }
+
+    public static BaseDAO getInstance(String dbName){
+        BaseDAO instance = instances.get(dbName);
+        if(instance == null){
+            instance = new BaseDAO();
+            instance.getConn(dbName);
+            instance.dbName = dbName;
+            instances.put(dbName, instance);
+            return instance;
+        }else{
+            return instance;
+        }
+    }
+
+    /**
+     *
+     * @return 获得数据库连接
+     * @throws java.sql.SQLException
+     * @throws ClassNotFoundException
+     */
+    private void getConn(String dbName) {
+        try {
+            Class.forName(DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            String connString = URL + dbName + "?characterEncoding=UTF-8";
+            conn = DriverManager.getConnection(connString, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close(){
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    protected void finalize() throws Throwable {
+        for(Map.Entry<String,BaseDAO> entry : instances.entrySet()){
+            entry.getValue().close();
+        }
+        instances = null;
+    }
+
+    /**
+     * 执行增、删、改SQL语句
+     * @param sql sql语句
+     * @param param 值集
+     * @param type 值类型集
+     * @return 受影响的行数
+     */
+    public int executeUpdate(String sql, Object[] param, int[] type) {
+
+        int rows = 0;
+        PreparedStatement prsts = null;
+        try {
+            prsts = conn.prepareStatement(sql);
+            for (int i = 1; i <= param.length; i++) {
+                prsts.setObject(i, param[i - 1], type[i - 1]);
+            }
+            rows = prsts.executeUpdate();
+            prsts.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return rows;
+    }
+
+    public int executeUpdate(String sql){
+        Object[] param = new Object[0];
+        int[] type = new int[0];
+        return executeUpdate(sql,param, type);
+    }
+
+    public List<Map<String,Object>> executeQuery(String sql){
+        return this.executeQuery(sql,null,null);
+    }
+
+    /**
+     * 执行查询SQL语句
+     * @param sql sql语句
+     * @param param 值集
+     * @param type 值类型集
+     * @return 结果集
+     */
+    public List<Map<String,Object>> executeQuery(String sql, Object[] param, int[] type) {
+        ResultSet rs = null;
+        List<Map<String,Object>> list = null;
+        PreparedStatement prsts = null;
+        try {
+            prsts = conn.prepareStatement(sql);
+            if(param != null) {
+                for (int i = 1; i <= param.length; i++) {
+                    prsts.setObject(i, param[i - 1], type[i - 1]);
+                }
+            }
+            rs = prsts.executeQuery();
+            list = new ArrayList<Map<String, Object>>();
+            ResultSetMetaData rsm = rs.getMetaData();
+            while (rs.next()) {
+                Map<String,Object> map = new HashMap<String,Object>();
+                for (int i = 1; i <= rsm.getColumnCount(); i++) {
+                    map.put(rsm.getColumnName(i), rs.getObject(rsm.getColumnName(i)));
+                }
+                list.add(map);
+            }
+            rs.close();
+            prsts.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    public static void main(String[] args){
+        BaseDAO base1 = BaseDAO.getInstance("cano");
+        BaseDAO base2 = BaseDAO.getInstance("cano");
+
+        BaseDAO base3 = BaseDAO.getInstance("duoguo");
+
+    }
+}
